78a7d3b460e35280337c5fb1c888d06f7bbfcf1b
PratyushRast2k3 <pratushx2003@gmail.comgit config --global user.email pratushx2003@gmail.com>
2024-03-17 12:47:40 +0530

    final commit1

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,36 +1,86 @@
+import React from 'react'
 
 function Calculator() {
-  function handleNumbers(){
-    
+  const [ calc,setCalc] = React.useState({
+    current:"0",
+    total:"0",
+    isInitial: true, 
+    preOp:""
+  });
+  function handleNumbers(value){
+  let newValue = value;
+  if(!calc.isInitial){
+  newValue = calc.current + value;
   }
-  function handleOperators(){
+    setCalc({current:newValue,total:calc.total,isInitial:false, preOp:calc.preOp});
 
   }
+  function doCalculation(){
+    let total = parseInt(calc.total);
+
+    switch(calc.preOp){
+      case "+":
+        total+= parseInt(calc.current);
+        break;
+      case "-":
+        total-= parseInt(calc.current);
+        break;
+      case "*":
+        total*= parseInt(calc.current);
+        break;
+      case "/":
+        total/= parseInt(calc.current);
+        break; 
+      default:
+        total = parseInt(calc.current);
+    }
+    return total;
+
+  }
+  function handleOperators(value){
+    const total=doCalculation();
+    setCalc({current:total.toString() ,total:total.toString(), isInitial:true,preOp:value});
+
+    
+  }
+  function handleClear(){
+    setCalc({
+      current:"0",
+      total:"0",
+      isInitial: true, 
+      preOp:""
+    })
+
+  }
+
+  function renderDisplay(){
+    return calc.current;
+  }  
   return (
     <div className="calculator">
-      <div className="display">0</div>
+      <div className="display">{renderDisplay()}</div>
       <CalcButton value="7" onClick={handleNumbers}/>
       <CalcButton value="8" onClick={handleNumbers}/>
       <CalcButton value="9" onClick={handleNumbers}/>
-      <CalcButton className='operators' value="/" onclick={handleOperators} />
+      <CalcButton className='operators' value="/" onClick={handleOperators} />
       <CalcButton value="4" onClick={handleNumbers}/>
       <CalcButton value="5" onClick={handleNumbers}/>
       <CalcButton value="6" onClick={handleNumbers}/>
-      <CalcButton className='operators' value="*" onclick={handleOperators}/>
+      <CalcButton className='operators' value="*" onClick={handleOperators}/>
       <CalcButton value="1" onClick={handleNumbers}/>
       <CalcButton value="2" onClick={handleNumbers}/>
       <CalcButton value="3" onClick={handleNumbers}/>
-      <CalcButton className='operators' value="-" onclick={handleOperators}/>
-      <CalcButton value="C"/>
+      <CalcButton className='operators' value="-" onClick={handleOperators}/>
+      <CalcButton value="C" onClick={handleClear}/>
       <CalcButton value="0" onClick={handleNumbers}/>
-      <CalcButton value="="/>
-      <CalcButton className='operators' value="+" />
+      <CalcButton value="=" onClick={handleOperators}/>
+      <CalcButton className='operators' value="+" onClick={handleOperators}/>
     </div>
   );
 }
 function CalcButton(props) {
   return (
-    <button className={props.className} onClick={props.onClick}>{props.value}</button>
+    <button className={props.className} onClick={()=> props.onClick(props.value)}>{props.value}</button>
   );
 }
 
